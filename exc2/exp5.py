1e9e490daca9c770deb61062bc196654d931895a
Bubbazz <15877918+bubbazz@users.noreply.github.com>
2021-06-12 14:57:05 +0200

    "richtig" es gibt ein fehler mit paar singular matrizen

--- exc2/exp5.py
+++ exc2/exp5.py
@@ -9,9 +9,8 @@ N = 3
 data = np.loadtxt(os.path.abspath("dataSets/gmm.txt"))
 means = [(random.uniform(-8, 8), random.uniform(-8, 8)) for i in range(N)]
 covs = [np.identity(2) for i in range(N)]
+# pi -> sum pi == 1
 mixing_coefficients = [1/N for i in range(3)]
-struc = [(means[0], d, multivariate_normal.pdf(d, means[0]))
-         for d in data[0:10]]
 # x, y = np.random.multivariate_normal(means[0], np.identity(2), 5000).T
 plt.scatter(data[:, 0], data[:, 1])
 
@@ -22,6 +21,8 @@ def Expectation(means, covs, mix_coefs, X):
     # 2 : x_2 : [pi_1*N(x_2|mu_1,cov_1),pi_2*N(x_2|mu_2,cov_2),pi_3*N(x_2|mu_3,cov_3)]
     all_we_need = [[mix_coef*multivariate_normal.pdf(x=x, mean=mean, cov=cov)
                     for mean, cov, mix_coef in zip(means, covs, mix_coefs)] for x in X]
+    # [pi_1*N(x_1|mu_1,cov_1)/sum(pi_1*N(x_1|mu_1,cov_1),pi_2*N(x_1|mu_2,cov_2),pi_3*N(x_1|mu_3,cov_3)),
+    # pi_2*N(x_1|mu_2,cov_2)/sum(pi_1*N(x_1|mu_1,cov_1),pi_2*N(x_1|mu_2,cov_2),pi_3*N(x_1|mu_3,cov_3))]...
     alphas = [[x/sum(xs) for x in xs] for xs in all_we_need]
     return alphas
 
@@ -34,7 +35,8 @@ def Maximization(X, alpha_list):
                           for alphas, x in zip(alpha_list, X)])
     new_means = [sum(new_means[:, i])/Nks[i]
                  for i in range(len(new_means[0]))]
-    new_cov = np.array([[np.matrix(x-new_mean).T*np.matrix((x-new_mean)) for alpha, new_mean in zip(alphas, new_means)]
+    # vec*vec.T
+    new_cov = np.array([[alpha*np.matrix(x-new_mean).T*np.matrix((x-new_mean)) for alpha, new_mean in zip(alphas, new_means)]
                         for alphas, x in zip(alpha_list, X)])
     new_cov = [sum(new_cov[:, i])/Nks[i]
                for i in range(len(new_cov[0]))]
@@ -59,6 +61,7 @@ def EMAlgo(means, covs, mix_coefs, steps):
 
 circle_x = np.arange(-1, 1, 0.01)
 cirlce_y = [np.sqrt(1-x**2) for x in circle_x]
+# [COV_i*(x,y)+mu_i]
 #plt.plot(circle_x, cirlce_y)
 EMAlgo(means, covs, mixing_coefficients, 10)
 plt.show()
